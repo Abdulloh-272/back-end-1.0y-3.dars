50571f18266eb842703d912334eac4b2b68a477b
Abdulloh-272 <167763951+Abdulloh-272@users.noreply.github.com>
2024-08-29 17:12:41 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+
+
+hayvon1 = input('Hayvon: ')
+ogirligi1 = int(input('Ogirligi: ')) 
+rangi1 = input('Rangi: ')
+yoshi1 = int(input('Yoshi: ')) 
+ismi1 = input('Ismi: ')
+
+print(f' Hayvon: {hayvon1}  Ogirligi: {ogirligi1},kg Rangi: {rangi1} Yoshi: {yoshi1},yil Ismi: {ismi1}')
+
+hayvon2 = input('Hayvon: ')
+ogirligi2 = int(input('Ogirligi: ')) 
+rangi2 = input('Rangi: ')
+yoshi2 = int(input('Yoshi: ')) 
+ismi2 = input('Ismi: ')
+
+print(f' Hayvon: {hayvon2}  Ogirligi: {ogirligi2} Rangi: {rangi2} Yoshi: {yoshi2} Ismi: {ismi2}')
+
+
+hayvon3 = input('Hayvon: ')
+ogirligi3 = int(input('Ogirligi: ')) 
+rangi3 = input('Rangi: ')
+yoshi3 = int(input('Yoshi: ')) 
+ismi3 = input('Ismi: ')
+
+print(f' Hayvon: {hayvon3}  Ogirligi: {ogirligi3} Rangi: {rangi3} Yoshi: {yoshi3} Ismi: {ismi3}')
+
+
+hayvon4 = input('Hayvon: ')
+ogirligi4 = int(input('Ogirligi: ')) 
+rangi4 = input('Rangi: ')
+yoshi4 = int(input('Yoshi: ')) 
+ismi4 = input('Ismi: ')
+
+print(f' Hayvon: {hayvon4}  Ogirligi: {ogirligi4} Rangi: {rangi4} Yoshi: {yoshi4} Ismi: {ismi4}')
+
+
